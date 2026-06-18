8adf209014e5067e8c59d6cf66e657207d4e094c
Datang Capital <datangcapital@example.com>
2026-06-18 13:13:46 +0800

    Update copyright year to 2020

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -64,7 +64,7 @@
       </div>
       
       <div class="border-t border-gray-700 mt-8 pt-8 text-center text-gray-400">
-        <p>&copy; 2024 Datang Capital Pty Ltd. All rights reserved.</p>
+        <p>&copy; 2020 Datang Capital Pty Ltd. All rights reserved.</p>
       </div>
     </div>
   </footer>
